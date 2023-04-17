59ad12f4b2d14e03328f2ebd49b0aeee27418877
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-17 15:41:51 -0400

    role service impl added

---
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -0,0 +1,42 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
+import com.cydeo.repository.RoleRepository;
+import com.cydeo.service.RoleService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
+
+    @Override
+    public List<RoleDTO> listAllRoles() {
+
+        // Controller called me and requested all RoleDTO, so it can show in the drop-down in the ui
+        // I need to make a call to db and get all the roles from table
+        // Go to repository find a service which gives me the roles from db
+
+        List<Role> roleList = roleRepository.findAll();
+
+        // to convert Role to RoleDTO, we add a mapper dependency from library, and so mapper package
+
+         return roleList.stream().map(roleMapper::convertToDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public RoleDTO findById(Long id) {
+        return null;
+    }
+}
