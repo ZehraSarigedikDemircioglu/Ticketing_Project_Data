1440a30c14cdb1d8f0cfd93f00872d0e5d49747a
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-17 15:45:07 -0400

    runner updated

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,25 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication // this includes @Configuration
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
+
     }
 
+    // Add bean in the container through @Bean annotation since mapper dependency from library.
+    // external => @Bean
+
+    // ModelMapper modelMapper = new ModelMapper(); => tightly-coupled
+    // to be loosely-coupled, we use as below
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
