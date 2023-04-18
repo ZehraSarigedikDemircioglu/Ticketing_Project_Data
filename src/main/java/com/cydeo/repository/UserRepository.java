6f963b1d8d2f02ca3d862dcfe771afc15cd2e83f
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-18 15:51:30 -0400

    @Transactional added for delete method

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -3,9 +3,14 @@ package com.cydeo.repository;
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // get user based on username
     User findByUserName(String username);
+    @Transactional // before we add this annotation, it gave me "transaction" error, it needs to be "rollback"
+            // if all steps are successfully completed
+            // rollback means if any error happened in any step, everything is going back.
     void deleteByUserName(String username);
 }
