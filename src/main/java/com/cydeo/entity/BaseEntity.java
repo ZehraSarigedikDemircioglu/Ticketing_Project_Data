e4b7f9520ae4f7a1144cf8abfd4db7a3867b10da
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-19 14:30:12 -0400

    added a delete field

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -16,6 +16,7 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private boolean isDeleted = false; // add this field after deleteByUserName() method since in the table, they were deleted and we do not want to delete in the db.
     private LocalDateTime insertDateTime;
     private Long insertUserId;
     private LocalDateTime lastUpdateDateTime;
