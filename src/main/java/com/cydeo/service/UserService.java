0dcfc136e050132d7e1a78c14088ac03215f782f
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-19 17:11:34 -0400

    added listAllByRole method

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -13,4 +13,5 @@ public interface UserService {
     UserDTO update(UserDTO user);
     void delete(String username); // added this method later to make sure we want to save "deleted username" in the db.
     // to prevent confliction, i created seperate this delete method instead of using deleteByUserName(String username)
+    List<UserDTO> listAllByRole(String role);
 }
