4c89b4b50b4961af20019795bdad1df4b52ae405
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-19 15:25:56 -0400

    added @Column annotations for PrePersist and PreUpdate

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -14,10 +14,16 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Boolean isDeleted = false; // add this field after deleteByUserName() method since in the table, they were deleted and we do not want to delete in the db.
+    @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
+    // when we add PrePersist and PreUpdate method, and made changes in the UserList, it became null those fields.
+    // So, we add @Column(nullable = false, updatable = false), keep the info for creation time, to see db not null
 
     @PrePersist
     private void onPrePersist() {
