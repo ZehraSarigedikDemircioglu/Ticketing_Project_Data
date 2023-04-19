e5a4ba99219cb1caec032d08bf8a5e140f6d3eee
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-19 15:46:01 -0400

    added project service

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,14 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    ProjectDTO getByProjectCode(String code);
+    List<ProjectDTO> listAllProjects();
+    void save(ProjectDTO project);
+    void update(ProjectDTO project);
+    void delete(String code);
+}
