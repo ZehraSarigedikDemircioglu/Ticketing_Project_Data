a3f68a2a1ecf19516d60422f4396c330140866c4
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-19 17:21:41 -0400

    impl added

---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,48 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.repository.ProjectRepository;
+import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
+
+    @Override
+    public ProjectDTO getByProjectCode(String code) {
+
+        return null;
+    }
+
+    @Override
+    public List<ProjectDTO> listAllProjects() {
+        return projectRepository.findAll().stream().map(projectMapper::convertToDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public void save(ProjectDTO project) {
+
+    }
+
+    @Override
+    public void update(ProjectDTO project) {
+
+    }
+
+    @Override
+    public void delete(String code) {
+
+    }
+}
