a90799dadd889fad631a6478633d8afa239c9b9f
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-21 00:02:15 -0400

    added update method

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -11,4 +11,5 @@ public interface ProjectService {
     void save(ProjectDTO project);
     void update(ProjectDTO project);
     void delete(String code);
+    void complete(String projectCode);
 }
