99078a34246375177bc0427c104923f3a5a395b5
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-24 19:38:26 -0400

    task service added

---
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -0,0 +1,14 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.TaskDTO;
+
+import java.util.List;
+
+public interface TaskService {
+
+    List<TaskDTO> listAllTasks();
+    void save(TaskDTO task);
+    void update(TaskDTO task);
+    void delete(Long id);
+    TaskDTO getById(Long id);
+}
