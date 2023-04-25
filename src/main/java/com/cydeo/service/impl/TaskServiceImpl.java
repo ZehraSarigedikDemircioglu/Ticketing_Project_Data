36f8bbe5163b7abc9744d1043808bca5bceb5229
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-25 11:15:44 -0400

    added delete method impl

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -3,7 +3,6 @@ package com.cydeo.service.impl;
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
-import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
 import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,16 +41,15 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void update(TaskDTO task) {
 
-//        // Find current user
-//        Task task1 = taskRepository.findById(task.getId());
-//        // Map update user dto to entity object
-//        Task convertedTask = taskMapper.convertToEntity(task);
-//        // Set id to the converted object
-//        convertedTask.setId(task1.getId());
-//        // Save the updated user in the db
-//        taskRepository.save(convertedTask);
-//        return findByUserName(task.getId());
-
+        // Find current user
+        Optional<Task> task1 = taskRepository.findById(task.getId());
+        // Map update user dto to entity object
+        Task convertedTask = taskMapper.convertToEntity(task);
+        // Set id to the converted object
+        convertedTask.setTaskStatus(task1.get().getTaskStatus());
+        convertedTask.setAssignedDate(task1.get().getAssignedDate());
+        // Save the updated user in the db
+        taskRepository.save(convertedTask);
     }
 
     @Override
