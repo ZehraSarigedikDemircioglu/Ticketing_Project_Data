0424a882878dd07d9bec010d309c79df0cb3c6fa
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-25 12:46:44 -0400

    listAllProjectDetails method added

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -12,4 +12,6 @@ public interface ProjectService {
     void update(ProjectDTO project);
     void delete(String code);
     void complete(String projectCode);
+
+    List<ProjectDTO> listAllProjectDetails();
 }
