8ad7fbad2e37664143d9f01b953e5e73832a75c7
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-27 11:01:52 -0400

    deleteByProject method added

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
 
 import java.util.List;
@@ -11,4 +12,8 @@ public interface TaskService {
     void update(TaskDTO task);
     void delete(Long id);
     TaskDTO getById(Long id);
+    int totalNonCompletedTask(String projectCode);
+    int totalCompletedTask(String projectCode);
+
+    void deleteByProject(ProjectDTO project);
 }
