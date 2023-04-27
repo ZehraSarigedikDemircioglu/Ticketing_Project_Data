0ab8a5b93db8f1ef8a253292712e5a019d6e2797
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-27 17:38:31 -0400

    edited method impl using MapperUtil

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,6 +2,7 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.MapperUtil;
 import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
@@ -15,10 +16,12 @@ public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
     private final RoleMapper roleMapper;
+    private final MapperUtil mapperUtil;
 
-    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper, MapperUtil mapperUtil) {
         this.roleRepository = roleRepository;
         this.roleMapper = roleMapper;
+        this.mapperUtil = mapperUtil;
     }
 
     @Override
@@ -32,7 +35,11 @@ public class RoleServiceImpl implements RoleService {
 
         // to convert Role to RoleDTO, we add a mapper dependency from library, and so mapper package
 
-         return roleList.stream().map(roleMapper::convertToDTO).collect(Collectors.toList());
+//         return roleList.stream().map(roleMapper::convertToDTO).collect(Collectors.toList());
+
+        // we put MapperUtil class to convert using gneric, either can be used for the implementation.
+        return roleList.stream().map(role -> mapperUtil.convert(role, new RoleDTO())).collect(Collectors.toList()); // 1.WAY
+//        return roleList.stream().map(role -> mapperUtil.convert(role, RoleDTO.class)).collect(Collectors.toList()); // 2.WAY
     }
 
     @Override
