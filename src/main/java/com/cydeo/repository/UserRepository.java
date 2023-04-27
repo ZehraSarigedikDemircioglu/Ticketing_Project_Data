08a2eb2b6dbade511a226fb9ada130182522b5f0
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-27 16:07:25 -0400

    added query with isDeleted

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -7,13 +7,20 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // removed @Where(clause = "is_deleted=false") from User entity, since it gave null using
+    // SELECT * FROM users WHERE id = 4 AND is_deleted = false; could not detect id number
+    // so, the easiest way to remove that annotation, and get derive query with deleted as below
+
+    List<User> findAllByIsDeletedOrderByFirstNameDesc(Boolean deleted);
+    // if it is true, all users if they are deleted, or the other way
 
     // get user based on username
-    User findByUserName(String username);
+    User findByUserNameAndIsDeleted(String username, Boolean deleted);
     @Transactional // before we add this annotation, it gave me "transaction" error, it needs to be "rollback"
             // if all steps are NOT successfully completed
             // rollback means if any error happened in any step, everything is going back.
     void deleteByUserName(String username);
 
-    List<User> findByRoleDescriptionIgnoreCase(String description);
+    List<User> findByRoleDescriptionIgnoreCaseAndIsDeleted(String description, Boolean deleted);
+
 }
