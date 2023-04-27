a87ff296aa3bace0c3ac841a6a0ac55eac600a9e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-27 15:06:03 -0400

    method impl added

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -109,4 +109,11 @@ public class ProjectServiceImpl implements ProjectService {
             return obj;
         }).collect(Collectors.toList());
     }
+
+    @Override
+    public List<ProjectDTO> listAllNonCompletedByAssignedManager(UserDTO assignedManager) {
+        List<Project> projects = projectRepository
+                .findAllByProjectStatusIsNotAndAssignedManager(Status.COMPLETE, userMapper.convertToEntity(assignedManager));
+        return projects.stream().map(projectMapper::convertToDTO).collect(Collectors.toList());
+    }
 }
