32c49dfea0f7412a78b8f20b674cc615fddf1875
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-27 16:06:49 -0400

    removed unused hard delete method

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -9,7 +9,7 @@ public interface UserService {
     List<UserDTO> listAllUsers();
     UserDTO findByUserName(String username);
     void save(UserDTO user);
-    void deleteByUserName(String username);
+//    void deleteByUserName(String username); // deleted this method since it is hard delete so no need anymore
     UserDTO update(UserDTO user);
     void delete(String username); // added this method later to make sure we want to save "deleted username" in the db.
     // to prevent confliction, i created seperate this delete method instead of using deleteByUserName(String username)
