bba88a75c310ac062d5cde2bb2e88088b98a1282
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-27 11:34:17 -0400

    updated

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -84,6 +84,8 @@ public class ProjectServiceImpl implements ProjectService {
         Project project = projectRepository.findByProjectCode(projectCode);
         project.setProjectStatus(Status.COMPLETE);
         projectRepository.save(project);
+
+        taskService.completeByProject(projectMapper.convertToDTO(project));
     }
 
     @Override
